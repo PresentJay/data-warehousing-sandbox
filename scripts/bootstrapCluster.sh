82cc388cbc5c5dabd4bb212d2365e007c918aa8b
Present_Jay.Dev <Presentj94@gmail.com>
2022-06-08 18:09:42 +0900

    feat(cluster): 클러스터 코드 정리 완료

--- scripts/bootstrapCluster.sh
+++ scripts/bootstrapCluster.sh
@@ -11,16 +11,15 @@ chmod g-r config/kubeconfig.yaml
 ### Ingress-Nginx 설치 (클러스터 내 트래픽 관리) ###
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update && \
-    helm install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace
+    helm install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace --version "4.1.3"
 
 ### Longhorn Storage 설치 (클러스터 내 가상 스토리지 관리) ###
 helm repo add longhorn https://charts.longhorn.io
 helm repo update && \
-    helm upgrade --install longhorn longhorn/longhorn --namespace longhorn-system --set csi.kubeletRootDir=/var/lib/kubelet --create-namespace
+    helm upgrade --install longhorn longhorn/longhorn --namespace longhorn-system --set csi.kubeletRootDir=/var/lib/kubelet --create-namespace --version "1.2.4"
 
 ### Kubernetes Dashboard 설치 (클러스터 모니터링 도구) ###
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm repo update && \
-    helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --namespace kubernetes-dashboard --create-namespace
-
+    helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --namespace kubernetes-dashboard --create-namespace --version "5.4.1"
 
